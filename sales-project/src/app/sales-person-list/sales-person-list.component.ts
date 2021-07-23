942026e7b01568b87bf0733df0809df4dd4ba698
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-23 15:37:18 +0200

    Add table with content from salesPersonList

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
-
+  
+  // create an array of sales person
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "some1@gmail.com", 5000),
+    new SalesPerson("John", "Doe", "some2@gmail.com", 7000),
+    new SalesPerson("Clair", "Murphy", "some3@gmail.com", 9000),
+    new SalesPerson("Mai", "Troung", "some4@gmail.com", 6000),
+  ];
   constructor() { }
 
   ngOnInit(): void {
